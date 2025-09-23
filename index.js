3be95a0ffa9bd639b3cf63f8d53601c8a596b8de
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-09-23 19:15:21 -0300

    added socket-io

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require("express");
 const sequelize = require("./src/config/database");
 const User = require("./src/models/User");
 const Iphone = require("./src/models/Iphone");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 const port = 3000;
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
 sequelize
   .sync({ alter: true }) // Cria/atualiza as tabelas no banco de dados
   .then(() => {
-    app.listen(port, () =>
+    httpServer.listen(port, () =>
       console.log(
         `Database connected successfully and app listening on port ${port}`
       )
